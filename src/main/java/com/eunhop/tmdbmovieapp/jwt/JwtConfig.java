63aaeb990c5c7fd02ce35c41df7bad9e4fc53253
Eunhop <thffoa99@gmail.com>
2024-05-23 04:29:38 +0900

    @Configuration 으로 변경

--- src/main/java/com/eunhop/tmdbmovieapp/jwt/JwtConfig.java
+++ src/main/java/com/eunhop/tmdbmovieapp/jwt/JwtConfig.java
@@ -2,13 +2,13 @@ package com.eunhop.tmdbmovieapp.jwt;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * `@Value` 애노테이션을 사용하여 주입된 값을 final 필드로 만들어 static 메소드에서 사용하려면,
  * 이를 관리하는 별도의 클래스를 만들고 싱글톤 패턴을 활용하여 필드 값을 접근하도록 합니다.
  */
-@Component
+@Configuration
 public class JwtConfig {
 
   // static 메소드에서 사용하기 위한 접근자 메소드
